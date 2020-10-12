1b7910d1e56d8144d956f58129720cd3819fc94a
Alex Sher <avstaim@yandex-team.ru>
2020-10-12 17:54:51 +0300

    Initial commit

---
+++ darkside/src/main/java/com/yandex/darkside/Darkside.kt
@@ -0,0 +1,6 @@
+package com.yandex.darkside
+
+/**
+ * TODO !!!
+ */
+object Darkside
---
+++ testapp/src/main/java/com/yandex/darkside/test/TestAppMainActivity.kt
@@ -0,0 +1,11 @@
+package com.yandex.darkside.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestAppMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
